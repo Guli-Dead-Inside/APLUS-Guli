a3271afaad932b4141c9375bd4fb9303dc62a543
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-01-20 19:45:56 +0300

    Sign

--- APLUS-Guli/APLUS-Guli/App.xaml.cs
+++ APLUS-Guli/APLUS-Guli/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APLUS;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace APLUS_Guli
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Signin());
         }
 
         protected override void OnStart()
---
+++ APLUS-Guli/APLUS-Guli/Signup.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APLUS-Guli
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Signup : ContentPage
+{
+    public Signup()
+    {
+        InitializeComponent();
+    }
+
+    private async void SignIntn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Signup());
+    }
+}
+}
